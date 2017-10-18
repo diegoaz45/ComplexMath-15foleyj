600da859405b4a461198553bb397f8aae09a8b74
15foleyj <jfoley9@myvu.vinu.edu>
2017-10-18 18:56:32 +0000

    Finalized Document

--- seriescircuitcalculations.py
+++ seriescircuitcalculations.py
@@ -4,6 +4,59 @@
 # Rev 2 adds polar to rectangular conversions and vice versa.
 # Includes basic series circuit calculations.
 
+
+def complex_add(complex_a, complex_b):
+    real_answer = complex_a[0] + complex_b[0]
+    imag_answer = complex_a[1] + complex_b[1]
+    return real_answer, imag_answer
+
+
+# Expects complex numbers in magnitude, phase. Returns the difference of B from A
+
+def complex_subtract(complex_a, complex_b):
+    real_answer = complex_a[0] - complex_b[0]
+    imag_answer = complex_a[1] - complex_b[1]
+    return real_answer, imag_answer
+
+
+# Expects complex numbers in magnitude, phase. Returns the quotient of B from A
+
+def complex_division(complex_a, complex_b):
+    real_answer = complex_a[0] / complex_b[0]
+    imag_answer = complex_a[1] - complex_b[1]
+    return real_answer, imag_answer
+
+
+# Expects complex numbers in magnitude, phase. Returns the product of A and B
+
+def complex_multiplication(complex_a, complex_b):
+    real_answer = complex_a[0] * complex_b[0]
+    imag_answer = complex_a[1] + complex_b[1]
+    return real_answer, imag_answer
+
+
+# Rectangular to Polar. Returns in (r, theta)
+
+def rect_to_polar(x, y):
+    angle = math.atan((y/x))
+    angle = angle * (180/pi)
+    magnitude = (math.sqrt((x*x)+(y*y)))
+    answer = magnitude, angle
+    return answer
+
+# Polar to Rectangular. Returns the X & Y lengths.
+
+def polar_to_rect(polar_num):
+    y = polar_num[0] * (math.sin(polar_num[1] * pi/180))
+    x = polar_num[0] * (math.cos(polar_num[1] * pi/180))
+    rect = x, y
+    return rect
+
+# Magnitude function 
+def magnitude(number):
+    absolute = math.sqrt((number[0] * number[0]) + (number[1]* number[1]))
+    return absolute
+
 import math
 from math import pi 
 #Recieve all information from user. 
@@ -35,7 +88,19 @@ print('1/(', omega,'*',capacitor_value1)
 capacitance = (1/(omega*inductor_value1))
 print('Capacitance is', +capacitance)
 
+
 #To Find Impedance
+impedance = resistor_value1, (inductance + -capacitance)
+mag_impedance = magnitude(impedance)
+current = float(voltage) / float(mag_impedance)
+vr = current * resistor_value1
+vl = current * inductance
+vc = current * capacitance
+print('\nYour total impedance is: %.2f + %.2fj' % (impedance[0], impedance[1]))
+print('That means the magnitude of your impedance is: %.2f' % mag_impedance)
+print('Which then means your current is: %f A' % current)
+print('V(R) = %.2f, V(L) = %.2f, V(C) = %.2f' % (vr, vl, vc))
+
 
 
 
